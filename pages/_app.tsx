fe284ff5b812ca0cbccc6efc7cd26711c51d35f4
Felipe Gustavo <55498940+felipe-gustavo@users.noreply.github.com>
2021-10-02 20:32:59 -0300

    Add and setup Material-UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,32 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Head from 'next/head';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { useEffect } from 'react';
+import { AppProps } from 'next/dist/shared/lib/router/router';
+import theme from '../src/theme';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function MyApp(props: AppProps) {
+  const { Component, pageProps } = props;
+
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+
+    if (jssStyles && jssStyles.parentElement) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>Consulta Tabela FIPE</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
-export default MyApp
